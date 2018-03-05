67a6515bb4c915d1b0563e6e7ae35895e0f654cd
LengmanXD <thriangaming@gmail.com>
2018-03-05 20:15:12 +0100

    Initial commit

---
+++ Source/Beneath/BeneathHUD.h
@@ -0,0 +1,25 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once 
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "BeneathHUD.generated.h"
+
+UCLASS()
+class ABeneathHUD : public AHUD
+{
+	GENERATED_BODY()
+
+public:
+	ABeneathHUD();
+
+	/** Primary draw call for the HUD */
+	virtual void DrawHUD() override;
+
+private:
+	/** Crosshair asset pointer */
+	class UTexture2D* CrosshairTex;
+
+};
+
